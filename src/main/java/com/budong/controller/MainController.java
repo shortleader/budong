5284cf1021a69188346563a8b96b62f885b7d478
HyunwooKang <shortleader2@gmail.com>
2018-12-07 16:48:58 +0900

    Design big update

--- src/main/java/com/budong/controller/MainController.java
+++ src/main/java/com/budong/controller/MainController.java
@@ -3,15 +3,12 @@ package com.budong.controller;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.budong.model.dto.NewsDTO;
 import com.budong.service.NewsServiceImpl;
 
@@ -34,7 +31,8 @@ public class MainController {
     		SimpleDateFormat date = new SimpleDateFormat("yyyyMMdd");
     		param = date.format(today);
     	}
-    	ModelAndView mav= new ModelAndView("khw/index");
+    	
+    	ModelAndView mav= new ModelAndView("khw/main");
     	ArrayList<NewsDTO> list = new ArrayList<>();
     	String url = "https://land.naver.com/news/headline.nhn?bss_ymd="+param;
     	list = newsService.getTitle(url);
---
+++ web/resources/js/news.js
@@ -0,0 +1,43 @@
+		/* News 관련 ajax처리 JS */
+
+		/* 오늘 날짜 가져오는 JS */
+		function getToday(){
+			var today = new Date();
+			var dd = today.getDate();
+			var mm = today.getMonth()+1; //January is 0!
+			if(dd<10) {
+			    dd='0'+dd;
+			} 
+			if(mm<10) {
+			    mm='0'+mm;
+			}
+			var yyyy = today.getFullYear();
+			today = ''+yyyy + mm + dd;
+			return today;
+		}
+		
+		function getTitle(page){
+		var param = page;
+			$.ajax({
+		    url:'/budong-info/title.news',
+		    data : "param=" + param,
+		    dataType:'text',
+		    success:function(data){
+		 	   $("#test").html(data);
+		    },error:function(){alert("Error");}
+		})
+		}
+		 
+		function getContent(url, pos){
+			var param = url;
+			var position = pos;
+			   $.ajax({
+		           url:'/budong-info/content.news',
+		           data : "param=" + param+"&pos="+pos,
+		           dataType:'text',
+		           success:function(data){
+		        	   $("#test").html(data);
+		           },error:function(){alert("Error");}
+		       })
+			}
+	
